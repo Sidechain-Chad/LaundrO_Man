4fcaec1951d55b906dc9629d225a4f44b6715362
Augzin <augustin@jekyllandhide.co.za>
2025-07-10 20:52:26 +0200

    Home page design

---
+++ app/controllers/laundromats_controller.rb
@@ -0,0 +1,27 @@
+class LaundromatsController < ApplicationController
+  def index
+    @laundromats = Laundromat.all
+  end
+
+  def show
+    @laundromat = Laundromat.find(params[:id])
+    @reviews = @laundromat.reviews.includes(:user)
+    @review = Review.new
+  end
+
+  def new
+    @laundromat = Laundromat.new
+  end
+
+  def create
+    @laundromat = Laundromat.new(laundromat_params)
+    @laundromat.save
+    redirect_to laundromat_path(@laundromat)
+  end
+
+  private
+
+  def laundromat_params
+    params.require(:laundromat).permit(:name, :address, :phone_number)
+  end
+end
