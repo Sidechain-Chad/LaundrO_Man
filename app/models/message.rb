9fafb8751341cba1e19b4a3f1426b027b11ae7e9
Feifei Mabusha <f.mabusha@gmail.com>
2025-07-15 18:45:59 +0200

    chatting

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,14 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :order, dependent: :destroy
+  after_create_commit :broadcast_message
+
+  private
+
+  def broadcast_message
+    broadcast_append_to "order_#{order.id}_messages",
+                        partial: "messages/message",
+                        target: "messages",
+                        locals: { message: self }
+  end
 end
