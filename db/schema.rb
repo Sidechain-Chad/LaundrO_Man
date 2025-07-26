1bbb147cdc9c407501204b9c52f57ac88b4f92f2
Aphiwe Mvimbi <mvimbiaphiwe@gmail.com>
2025-07-26 12:56:27 +0200

    master

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_19_165647) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_26_102511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_19_165647) do
     t.bigint "user_id", null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "decription"
     t.index ["user_id"], name: "index_laundromats_on_user_id"
   end
 
