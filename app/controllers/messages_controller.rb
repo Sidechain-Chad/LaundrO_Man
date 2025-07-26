a74edeb3dad4eeb02c068c684d3304e2d1941301
Feifei Mabusha <f.mabusha@gmail.com>
2025-07-26 15:17:30 +0200

    fixin

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,6 +31,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_07_19_165647) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_07_26_130719) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,7 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_19_165647) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.text "content"
+    t.text "body"
     t.bigint "user_id", null: false
     t.bigint "order_id", null: false
     t.datetime "created_at", null: false
